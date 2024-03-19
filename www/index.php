a9c112196a418b4844247988753376cfede42b61
Beau.s <9isa.beau@gmail.com>
2024-03-19 12:33:10 +0100

    de index pagina beginnen en stylen

--- www/index.php
+++ www/index.php
@@ -1,8 +1,19 @@
-<?php
+<!DOCTYPE html>
+<head>
+    <link href="style.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <title>Sims4 Expansion packs</title>
+</head>
+
+<body>
+<div class="top-right-image">
+        <img src="c:\Users\isabe\OneDrive\Bureaublad\school blok3\front-end\images\plumbob.webp" alt="Top Right Image">
+    </div>
+    <?php
 
 require 'database.php';
 
-//de sql query
+//de sql query  
 $sql = "SELECT * FROM `welsh_eten`";
 $result = mysqli_query($conn,$sql);
 $welsh_eten = mysqli_fetch_all($result, MYSQLI_ASSOC);
@@ -12,3 +23,5 @@ $welsh_eten = mysqli_fetch_all($result, MYSQLI_ASSOC);
 <?php foreach($welsh_eten as $recept): ?>
   <p><?php echo $recept["naam_gerecht"] ?></p>
 <?php endforeach; ?>
+</body>
+
