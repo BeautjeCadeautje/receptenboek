2e468dd5cb2d0bb9e53508e79196011ffb42dcc1
Beau.s <9isa.beau@gmail.com>
2024-03-20 08:42:40 +0100

    nu heb ik ervoor gezorcht dat alle namen van elk gerecht onder elkaar apart staan

--- www/index.php
+++ www/index.php
@@ -1,12 +1,18 @@
-<!DOCTYPE html>
 <?php
+
+$sql = "SELECT * FROM `welsh_eten` LIMIT 9"; // Haal de eerste twee gerechten uit de database
 require 'database.php';
-$sql = "SELECT * FROM `welsh_eten`";
 $result = mysqli_query($conn, $sql);
-$welsh_eten = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
 ?>
 
+
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="style.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <title>RECEPTENBOEK</title>
@@ -23,15 +29,16 @@ $welsh_eten = mysqli_fetch_all($result, MYSQLI_ASSOC);
     </div>
 
     <div class="container">
-        <div class="box">
-            <?php foreach ($welsh_eten as $recept) : ?>
-                <p><?php echo $recept["naam_gerecht"] ?></p>
-            <?php endforeach; ?>
-
-            <img src="" alt="gerecht" style=" width:300px;height:400px;"></a><br>
-            <a href="https://www.ea.com/games/the-sims/the-sims-4/store/addons/the-sims-4-high-school-years" style="color:black;">RECEPT</a>
-        </div>
+        <?php while ($gerecht = mysqli_fetch_assoc($result)) : ?>
+            <div class="box">
+                <h1><?php echo $gerecht["naam_gerecht"] ?></h1>
+                <img src="" alt="gerecht" style=" width:300px;height:400px;"></a><br>
+                <a href="https://www.ea.com/games/the-sims/the-sims-4/store/addons/the-sims-4-high-school-years" style="color:black;">RECEPT</a>
+            </div>
+        <?php endwhile; ?>
     </div>
 
 
 </body>
+
+</html>
