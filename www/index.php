70063ba26d28481d71537bb0a9b2117c69e23227
Beau.s <9isa.beau@gmail.com>
2024-03-20 08:50:31 +0100

    ik heb een begin gemaakt aan de index pagina en heb een mag images gemaakt en ddaar een images in geplaatst

---
+++ www/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<head>
+    <link href="style.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <title>RECEPTENBOEK</title>
+</head>
+<body>
+    <div class="top-right-image">
+        <img src="" alt="Top Right Image">
+    </div>
+
+
+    <div class="top"> 
+        <a href="file:///C:/Users/isabe/OneDrive/Bureaublad/school%20blok3/front-end/extra%20page.html" style="color:white;">FAQ's</a>
+        <h1>Sims4 Expansion packs</h1>
+    </div>
+
+    <div class="container">
+        <div class="box">
+            
+        </div>
+        
+    </div>
+</body>
+<footer>
+    <div class="bottom">
+        <p>follow us on social media!</p><br>
+        <a href="https://www.instagram.com/" target="_blank" class="social-link"><i class="fab fa-instagram"></i></a>
+        <a href="https://www.facebook.com/" target="_blank" class="social-link"><i class="fab fa-facebook"></i></a>
+        <a href="https://www.youtube.com/" target="_blank" class="social-link"><i class="fab fa-youtube"></i></a>
+    </div>
+</footer>
