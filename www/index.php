408ecb80a4fc6aadab0ba0fb22ce6276237473ed
Beau.s <9isa.beau@gmail.com>
2024-03-21 08:30:38 +0100

    ik heb een achtergrond gebruikt om mijn voorpagina mooi te maken

--- www/index.php
+++ www/index.php
@@ -1,32 +1,39 @@
 <!DOCTYPE html>
+<html>
 <head>
-    <link href="css/index.css" rel="stylesheet"/>
+    <link href="css/index.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <title>RECEPTENBOEK</title>
 </head>
+
 <body>
-<div class="top-right-image">
-        <img src="images/Wales_national_football_team_logo.svg.png" alt="Top Right Image">
-    </div>
-
-
-    <div class="top"> 
-        <a href="file:///C:/Users/isabe/OneDrive/Bureaublad/school%20blok3/front-end/extra%20page.html" style="color:white;">FAQ's</a>
-        <h1>Sims4 Expansion packs</h1>
-    </div>
-
-    <div class="container">
-        <div class="box">
-            
+    <div class="background-image">
+        <div class="overlay">
+            <div class="overlay-content">
+                <!-- Plaats hier je tekst -->
+                <button><a href="http://localhost/recepten.php" style="color:white;">FAQ's</a><br></button>
+                <p>Welkom bij Receptenboek</p>
+            </div>
         </div>
-        
+        <div class="top-right-image">
+            <img src="images/Flag_of_Wales.svg.png" alt="Top Right Image">
+        </div>
+
+        <div class="top">
+            <a href="file:///C:/Users/isabe/OneDrive/Bureaublad/school%20blok3/front-end/extra%20page.html" style="color:white;">FAQ's</a><br>
+            <a href="file:///C:/Users/isabe/OneDrive/Bureaublad/school%20blok3/front-end/extra%20page.html" style="color:white;">CONTACT</a>
+        </div>
+
+       
     </div>
+
+    <footer>
+        <div class="bottom">
+            <p>follow us on social media!</p><br>
+            <a href="https://www.instagram.com/" target="_blank" class="social-link"><i class="fab fa-instagram"></i></a>
+            <a href="https://www.facebook.com/" target="_blank" class="social-link"><i class="fab fa-facebook"></i></a>
+            <a href="https://www.youtube.com/" target="_blank" class="social-link"><i class="fab fa-youtube"></i></a>
+        </div>
+    </footer>
 </body>
-<footer>
-    <div class="bottom">
-        <p>follow us on social media!</p><br>
-        <a href="https://www.instagram.com/" target="_blank" class="social-link"><i class="fab fa-instagram"></i></a>
-        <a href="https://www.facebook.com/" target="_blank" class="social-link"><i class="fab fa-facebook"></i></a>
-        <a href="https://www.youtube.com/" target="_blank" class="social-link"><i class="fab fa-youtube"></i></a>
-    </div>
-</footer>
+</html>
